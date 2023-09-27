80ae643dff7aa933626041f8638ee007e665df39
athomas2023 <97468398+athomas2023@users.noreply.github.com>
2023-09-27 17:31:20 -0400

    updates

---
+++ IFgame/Assets/Scripts/DecisionMangar.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class DecisionMangar : MonoBehaviour
+{
+
+    public TextMeshProUGUI ChoiceMeter;
+
+    private float choiceVar = 0f;
+    private string choiceDisplay;
+
+    public TextMeshProUGUI OverWriteText;
+
+
+    public string GreatEnding = "";
+    public string GoodEnding = "";
+    public string BadEnding = "";
+    
+    private void Start()
+    {
+        choiceDisplay = choiceVar.ToString();
+        ChoiceMeter.text = choiceDisplay;
+
+    }
+   public void Great()
+   {
+        choiceVar += 1f;
+        choiceDisplay = choiceVar.ToString();
+        ChoiceMeter.text = choiceDisplay;
+
+        OverWriteText.text = GreatEnding;
+
+   }
+
+   public void Good()
+   {
+
+        choiceVar += 0.5f;
+        choiceDisplay = choiceVar.ToString();
+        ChoiceMeter.text = choiceDisplay;
+
+        OverWriteText.text = GoodEnding;
+   }
+
+   public void Bad()
+   {
+        choiceVar += 1f;
+        choiceDisplay = choiceVar.ToString();
+        ChoiceMeter.text = choiceDisplay;
+
+        OverWriteText.text = BadEnding;
+   }
+}
